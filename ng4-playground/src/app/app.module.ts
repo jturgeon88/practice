57cf315df2e06f0688b837e05831dcd6095240a3
Josh Turgeon <jturgeon88@gmail.com>
2018-11-13 08:53:46 -0600

    generate customers component

--- ng4-playground/src/app/app.module.ts
+++ ng4-playground/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MyNewComponentComponent } from './my-new-component/my-new-component.component';
+import { CustomersComponent } from './customers/customers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyNewComponentComponent
+    MyNewComponentComponent,
+    CustomersComponent
   ],
   imports: [
     BrowserModule
